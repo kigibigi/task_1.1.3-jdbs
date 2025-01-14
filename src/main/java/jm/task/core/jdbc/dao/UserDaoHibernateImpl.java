cd91f6221257f337c1637152bb4d3c4a6f9254c3
Boris <b.molchanov.90@gmail.com>
2025-01-14 23:37:29 +0300

    fix 14.01 hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -27,45 +27,62 @@ public class UserDaoHibernateImpl implements UserDao {
             )
             """;
 
+    // DDL - CREATE, DROP, TRUNCATE
     @Override
     public void createUsersTable() {
-        Session session = sessionFactory.openSession();
-        try {
+        try (Session session = sessionFactory.openSession()) {
+
             session.beginTransaction();
             session.createSQLQuery(CREATE_USER_TABLE_SQL).executeUpdate();
             session.getTransaction().commit();
 
-        } catch (PersistenceException ex) {
-            System.out.println("Table 'user' already exists");
-            session.getTransaction().rollback();
+        } catch (Exception ex) {
+            // ignore
         }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = sessionFactory.openSession();
-        try {
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
             session.createSQLQuery("DROP TABLE user").executeUpdate();
 
             session.getTransaction().commit();
-        } catch (PersistenceException ex) {
-            System.out.println("Table is not exist");
-            session.getTransaction().rollback();
+        } catch (Exception ex) {
+//            (PersistenceException ex)
         }
     }
 
     @Override
+    public void cleanUsersTable() {
+        try(Session session = sessionFactory.openSession())
+        {
+            session.beginTransaction();
+
+            session.createQuery("DELETE FROM User").executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    // DML - SELECT, INSERT, UPDATE, DELETE
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         User user = new User(name, lastName, age);
+
         Session session = sessionFactory.openSession();
         try {
             session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
         } catch (Exception ex) {
+            session.getTransaction().rollback();
             throw ex;
+        } finally {
+            session.close();
         }
     }
 
@@ -80,33 +97,27 @@ public class UserDaoHibernateImpl implements UserDao {
             session.getTransaction().commit();
         } catch (Exception ex) {
             throw ex;
+        } finally {
+            session.close();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = sessionFactory.openSession();
         List<User> users = new ArrayList<>();
 
-        session.beginTransaction();
-
-        users = (List<User>) session.createQuery("from User order by id").list();
-
-        session.getTransaction().commit();
-        return users;
-    }
-
-    @Override
-    public void cleanUsersTable() {
         Session session = sessionFactory.openSession();
         try {
             session.beginTransaction();
 
-            session.createQuery("DELETE FROM User").executeUpdate();
+            users = (List<User>) session.createQuery("from User order by id").list();
 
             session.getTransaction().commit();
         } catch (Exception ex) {
             throw ex;
+        } finally {
+            session.close();
         }
+        return users;
     }
 }
